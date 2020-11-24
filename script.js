e205c375777bd0a6470890ddf4e1c51194245541
Kristina <kashqar1117@gmail.com>
2020-11-24 17:41:23 -0600

    added arr

--- script.js
+++ script.js
@@ -4,3 +4,23 @@ var todoList = document.querySelector("#todo-list");
 var todoCountSpan = document.querySelector("#todo-count");
 
 var todos = ["Learn HTML", "Learn CSS", "Learn JavaScript"];
+
+
+function renderTodos(){
+    for (var i = 0;i < todos.length; i++){
+        // pcreate li emel
+       var li = document.createElement('li')
+        // add to text to li
+        li.innerText=todos[i]
+
+        
+        //apend it to ul"todolist
+        todoList.appendChild(li)
+        
+        
+        
+    }
+}
+
+
+renderTodos()
